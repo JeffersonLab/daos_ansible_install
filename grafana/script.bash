f0c77c41537a5132ba02dd7169fc7235b025a594
Xinxin Mei <xmei@jlab.org>
2024-02-23 11:38:57 -0500

    Update the script to save Grafana dashboard in json

--- grafana/script.bash
+++ grafana/script.bash
@@ -1,4 +1,6 @@
 # Bash command to save the Grafana dashboad into a json file.
 
 # Check $dashboard_id via UI.
+# "http://localhost:3000" is Grafana's address. Make sure it matches the real case.
+
 curl -u "${usr}:${psd}" -X GET http://localhost:3000/api/dashboards/uid/${dashboard_id} > $save_path.json
